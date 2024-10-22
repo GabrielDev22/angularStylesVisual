e9734fe803b80dbbaf129b74818671535516879e
Gabriel <gabrieltorresmora2@gmail.com>
2024-10-21 22:02:08 -0300

    guardando mejoras visuales en el tipo de pokemon, filtro del nombre de habilidades en español y estilos de color de los pokemons

--- src/app/home/section/section-details-pokemon/section-details-pokemon.component.ts
+++ src/app/home/section/section-details-pokemon/section-details-pokemon.component.ts
@@ -1,15 +1,17 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ExternalEndPointsAplicationService } from '../../external-end-points-aplication.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-section-details-pokemon',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './section-details-pokemon.component.html',
   styleUrls: ['./section-details-pokemon.component.css']
 })
 export class SectionDetailsPokemonComponent implements OnInit {
 
- pokemon : any;
  pokemonImage! : string;
  pokemonName! : string;
  pokemonExperience! : number;
@@ -19,10 +21,10 @@ export class SectionDetailsPokemonComponent implements OnInit {
  pokemonType2! : string;
  maxExperience = 500;
 
- pokemonAbilitiesName1: string = '';
- pokemonAbilitiesName2 : string = '';
+ pokemonAbilitiesName!: any;
  abilitiesContent : string[] = [];
- abilitieLanguague! : any;
+ typePokemonProgressBar! : string;
+ backgroundContainer! : string;
 
   constructor(
     private route : ActivatedRoute,
@@ -33,34 +35,63 @@ export class SectionDetailsPokemonComponent implements OnInit {
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
       const namePokemon = params['name'];
-      this.getPokemonByName(namePokemon, this.pokemon);
+      this.getPokemonByName(namePokemon);
     })
   }
 
-  getPokemonByName(pokemonName : string, pokemon: any){
+  getPokemonByName(pokemonName : string){
     this._externalEndPointApplication.getPokemonByName(pokemonName).subscribe(res => {
+      console.log(res);
       this.pokemonName = res.name;
       this.pokemonImage = res.sprites.other.dream_world.front_default;
       this.pokemonExperience = res.base_experience;
       this.pokemonHeight = res.height;
       this.pokemonWeight = res.weight;
       this.pokemonType = res.types[0].type.name;
+      this.getTypePokemonProgressBar(this.pokemonType);
       this.pokemonType2 = res.types[1].type.name;
-      this.pokemonAbilitiesName1 = res.abilities[0].ability.name;
-      this.pokemonAbilitiesName2 = res.abilities[1].ability.name;
+      this.pokemonAbilitiesName = res.abilities;
+      this.getPokemonAbility(this.pokemonAbilitiesName);
+      this.change.markForCheck();
     })
   }
 
-  getPokemonAbility(pokemonAbilityName : string){
-    this._externalEndPointApplication.getPokemonAbility(pokemonAbilityName).subscribe(res => {
-      console.log(res);
-      this.abilitiesContent = res.names;
-      this.abilitiesContent.forEach((item: any) => {
-        if(item.language && item.language.name === 'es'){
-          this.abilitieLanguague = item;
-        }
-      })
-    })
+  getTypePokemonProgressBar(pokemonType : string){
+    switch(pokemonType){
+      case 'fire':
+        this.typePokemonProgressBar = 'fire'
+        this.backgroundContainer = 'fire';
+        break;
+      case 'water':
+         this.typePokemonProgressBar = 'water'; 
+         this.backgroundContainer = 'water'; 
+         break;
+      case 'grass':
+         this.typePokemonProgressBar = 'grass';
+         this.backgroundContainer = 'grass';
+         break; 
+      default:
+          this.typePokemonProgressBar;
+          this.backgroundContainer;
+          break;
+    }
+    return this.typePokemonProgressBar;
+  }
+
+  getPokemonAbility(pokemonAbilityName : any){
+    this.abilitiesContent = [];
+    pokemonAbilityName.forEach((item: any) => {
+      this._externalEndPointApplication.getPokemonAbility(item.ability.name).subscribe(res => {
+        const abilityName = res.names;
+        const abilityFlavorTextEntries = res.flavor_text_entries;
+        const spanishAbility = abilityName.find((ability: any) => ability.language && ability.language.name === 'es');
+        const spanishFlavorTextEntreties = abilityFlavorTextEntries.find((flavorText : any) => flavorText.language && flavorText.language.name === 'es');
+        console.log(spanishFlavorTextEntreties);   
+        if(spanishAbility){
+             this.abilitiesContent.push(spanishAbility.name);
+           } 
+        });
+    });
   }
 
   getExperiencePercentage(): number{
